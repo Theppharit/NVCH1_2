47a2a8e8bf01607efef16b9bb7c79677e6019afd
nxghtz <dekdgamer348@gmail.com>
2025-10-28 08:51:16 +0700

    Update index.php

--- 38_Nonpawit/index.php
+++ 38_Nonpawit/index.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
+
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Nonpawit</title>
 </head>
+
+
+
 <body>
+
+    <h1>Nonpawit</h1>
+    <p>This is the Nonpawit page.</p>
     
 </body>
+
+
+
 </html>
