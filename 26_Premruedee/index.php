e031091f6cebee01af4f21ecb4edf4d365fac23f
prapapan24 <Prapapan6818@gmail.com>
2025-11-04 10:36:02 +0700

    .

--- 26_Premruedee/index.php
+++ 26_Premruedee/index.php
@@ -9,39 +9,39 @@
 <body>
 
 <nav class="navbar">
-    <a href="" class="a-navbar">หัวข้อที่ 1</a>
-    <a href="" class="a-navbar">หัวข้อที่ 2</a>
-    <a href="" class="a-navbar">หัวข้อที่ 3</a>
+    <a href="#title1" class="a-navbar">หัวข้อที่ 1</a>
+    <a href="#title2" class="a-navbar">หัวข้อที่ 2</a>
+    <a href="#title3" class="a-navbar">หัวข้อที่ 3</a>
 </nav>
 
 
 
 
     <!-- ส่วนหัว -->
-    <div class="div-title">
+    <div class="div-title" id="title 1">
         <h1 class="title-h1">Premruedee Supharoekmontha</h1>
     </div>   
 
     <!-- กล่องที่ 1 -->
-    <div class="box box-blue">
+    <div class="box box-blue" id="title 2">
         <h2 class="title-h2">Subtitle</h2>
         <p>My name is Premruedee Supharoekmontha but in online you can call me Premruedee it kinda good when you call me by this name</p>
     </div>
 
     <!-- กล่องที่ 2 -->
-    <div class="box box-yellow">
+    <div class="box box-yellow" id="title 3">
         <h2 class="title-h3">Pls sub</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio harum similique rem iste perferendis hic temporibus et obcaecati consequuntur nobis eius expedita placeat magni dolorum maxime molestias, vel numquam corporis?</p>
     </div>
 
     <!-- กล่องที่ 3 -->
-    <div class="box box-green">
+    <div class="box box-green" id="title 4">
         <h3 class="title-h4">Ms. Premruedee Supharoekmontha</h3>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates doloremque ipsam maxime sequi dolorem atque eveniet tenetur velit, reiciendis, blanditiis optio ad qui fugiat enim. Aperiam natus eum earum reiciendis.</p>
     </div>
    
     <!-- กล่องที่ 4 -->
-    <div class="box box-purple">
+    <div class="box box-purple" id="title 5">
         <h4 class="title-h5">Chompooo</h4>
     </div>
 
