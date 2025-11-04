f57ca0d07788164a5e7b41a092032cc812fbaae4
nanay999 <nay.thatphon@gmail.com>
2025-11-04 10:57:33 +0700

    +

--- 33.rattanachot thuengin/index.php
+++ 33.rattanachot thuengin/index.php
@@ -3,42 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>rattanachot thuengin</title>
+    <title>Thanawat Jakoh</title>
     <link rel="stylesheet" href="asset/style.css">
 </head>
 <body>
 
+<nav class="navbar">
+    <a href="#title1" class="a-navbar">หัวข้อที่ 1</a>
+    <a href="#title2" class="a-navbar">หัวข้อที่ 2</a>
+    <a href="#title3" class="a-navbar">หัวข้อที่ 3</a>
+</nav>
+
     <!-- ส่วนหัว -->
     <div class="div-title">
-        <h1 class="title-h1">rattanachot thuengin</h1>
+        <h1 class="title-h1" id="title1">rattanachot thuengin</h1>
     </div>
 
     <!-- กล่องที่ 1 -->
     <div class="box box-blue">
-        <h2 class="title-h2">Halo babyyy</h2>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, asperiores doloribus! Iste, repudiandae reprehenderit commodi quidem quod cumque dolorem deserunt officia.</p>
+        <h2 class="title-h2" id="title2">Helooo babyyyy</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aut sed iure nam a adipisci mollitia incidunt voluptatem ad! Minus, magnam? Ducimus laudantium magnam placeat quam mollitia deleniti veniam nam.
+        </p>
     </div>
 
     <!-- กล่องที่ 2 -->
     <div class="box box-yellow">
-        <h2 class="title-h3">hello wayyyyyy</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio incidunt accusamus deleniti nobis inventore? Quaerat accusantium ullam voluptate inventore incidunt, tempore minima explicabo obcaecati!</p>
+        <h2 class="title-h3" id="title3">wowwwww</h2>
+        <p></p>
     </div>
 
     <!-- กล่องที่ 3 -->
     <div class="box box-green">
-        <h3 class="title-h4"> blue fhinix</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui blanditiis voluptates, maxime sapiente ab alias sint aperiam quod.</p>
+        <h3 class="title-h4">blue phinix</h3>
+        <p>koko film go to banana</p>
     </div>
 
     <!-- กล่องที่ 4 -->
     <div class="box box-purple">
-        <h4 class="title-h5">Hello to gooooooo</h4>
+        <h4 class="title-h5">Helol wordd</h4>
     </div>
 
 </body>
 </html>
-
-
 
  
