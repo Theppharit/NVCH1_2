1b10858c88d95d4c62155d5416074239bf9a33b9
nanay999 <nay.thatphon@gmail.com>
2025-11-04 09:28:49 +0700

    +

--- 33.rattanachot thuengin/index.php
+++ 33.rattanachot thuengin/index.php
@@ -3,15 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>rattanachot</title>
+    <title>Thanawat Jakoh</title>
+    <link rel="stylesheet" href="asset/style.css">
 </head>
 <body>
- <h1 class="">h1</h1> 
- <p class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio reiciendis, voluptate ipsa ratione dolorem qui dolor excepturi facere neque vitae. Provident adipisci laboriosam, animi inventore doloremque iusto voluptate autem tenetur!
- <h2 class="">h2</h2>
- <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, nihil repudiandae. Molestiae, architecto quibusdam debitis quisquam cumque laborum nulla libero ipsum praesentium, quia saepe recusandae dolorem, ab velit vel sit!
- <h3 class="">h3</h3>
- <P class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis quidem ad ex accusantium iste sint officiis laborum libero aliquid soluta placeat dignissimos porro reiciendis, corporis rerum nisi illo officia cupiditate.
+
+    <!-- ส่วนหัว -->
+    <div class="div-title">
+        <h1 class="title-h1">rattanachot thuengin</h1>
+    </div>
+
+    <!-- กล่องที่ 1 -->
+    <div class="box box-blue">
+        <h2 class="title-h2">Halo babyyy</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, asperiores doloribus! Iste, repudiandae reprehenderit commodi quidem quod cumque dolorem deserunt officia.</p>
+    </div>
+
+    <!-- กล่องที่ 2 -->
+    <div class="box box-yellow">
+        <h2 class="title-h3">hello wayyyyyy</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio incidunt accusamus deleniti nobis inventore? Quaerat accusantium ullam voluptate inventore incidunt, tempore minima explicabo obcaecati!</p>
+    </div>
+
+    <!-- กล่องที่ 3 -->
+    <div class="box box-green">
+        <h3 class="title-h4">Ms. blue fhinix</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui blanditiis voluptates, maxime sapiente ab alias sint aperiam quod.</p>
+    </div>
+
+    <!-- กล่องที่ 4 -->
+    <div class="box box-purple">
+        <h4 class="title-h5">Hello to gooooooo</h4>
+    </div>
 
 </body>
 </html>
+
+
+
+ 
