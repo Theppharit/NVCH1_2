8cb0a26c5168f2b62032d7d61a50980cbfd323c1
suer0660851120-a11y <suer0660851120@gmail.com>
2025-11-04 09:53:19 +0700

    Update index.php
    tttt

--- 48_Phonnarin/index.php
+++ 48_Phonnarin/index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/style.css">
 
-    <title>Theppharit</title>
+    <title>Phonnarin</title>
 </head>
 
 
