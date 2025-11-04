cb96e3dd4e7f8e16042b4172e745924a699c96d8
Vasuthon <sirarmysdaphr@gmail.com>
2025-11-04 12:09:37 +0700

    wow

--- 47_Thitikan/index.php
+++ 47_Thitikan/index.php
@@ -1,8 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
-
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,9 +7,6 @@
 
     <title>Thitikan</title>
 </head>
-
-
-
 <body>
 
 
@@ -20,30 +14,38 @@
     <a href="#title1" class="a-navbar">หัวข้อที่1</a>
     <a href="#title2" class="a-navbar">หัวข้อที่2</a>
     <a href="#title3" class="a-navbar">หัวข้อที่3</a>
+    <a href="#title4" class="a-navbar">หัวข้อที่4</a>
 </nav>
 
-<div class="div-title" id="title1">
-<h1 class="title-h1">Thitikan Anchuen</h1>
-
+<!--ส่วนหัว-->
+<div class="div-title">
+<h1 class="title-h1" id="title1">Thitikan Anchuen</h1>
 </div>
 
-    <h2 class="title-h2">Subtitle</h2>
-
+<!--กล่องที่ 1-->
+<div class="box box-blue">
+    <h2 class="title-h2" id="title2">Subtitle</h2>
     <p1 class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate eum doloremque exercitationem iusto distinctio soluta. Similique perspiciatis quas quae sit quidem sint, quis ipsa quibusdam possimus cumque aut accusantium voluptatum!</p>
+</div>
 
-    <h2 class="title-h3">Pls sub</h2>
-
+<!--กล่องที่ 2-->
+<div class="box box-blue">
+    <h2 class="title-h3" id="title3">Pls sub</h2>
     <p2 class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro iste dicta id, ullam maxime inventore eaque nisi molestias harum voluptate qui veritatis hic exercitationem. Inventore animi magni eos voluptates in?</p>
+</div>
 
-    <h3 class="title-h4">Ms.Thitikan Anchuen</h3>
-
+<!--กล่องที่ 3-->
+<div class="box box-blue">
+    <h3 class="title-h4" id="title4">Ms.Thitikan Anchuen</h3>
     <p3 class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo a voluptas est natus voluptatum voluptatem itaque, quas id molestias? Corrupti doloribus accusantium ipsam aspernatur non. Ea, rerum accusantium? Et, perferendis.</p>
-
-    <p4 class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, dolores ipsa praesentium laboriosam alias enim veritatis inventore ducimus illum nisi optio ab temporibus rem placeat debitis. Cum sit labore qui!</p>
+</div>
+    
+<!--กล่องที่ 4-->
+<div class="box box-blue">
+<h4 class="title-h5" id="title4">Gungging</h4>
+</div>
 
 </body>
-
-
 
 </html>
 
