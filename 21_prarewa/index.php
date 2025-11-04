ab213223346477bead5e6f126c63b251a7c8b794
prarewa2550-prog <prarewa2550@gmail.com>
2025-11-04 11:05:49 +0700

    .

--- 21_prarewa/index.php
+++ 21_prarewa/index.php
@@ -3,36 +3,42 @@
 
 
 
-
-<head> 
-    <meta charset="UTF-8"
-    <meta name="viewport" contenet"width=device-width, initial-scale=1.0">
-    <link rei="stylesheet" href="./assets/style.css">
-
-    <title>prarewa</title>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/style.css">
 
+    <title>Prarewa</title>
+</head>
 
 <body>
+    
 
-<div class="div-title">
-<h1 class"title-h1">prarewa Rerkcharern</h1>
+
+<nav class="navbar">
+    <a href="#title1" class="a-navbar">หัวข้อที่ 1</a>
+    <a href="#title2" class="a-navbar">หัวข้อที่ 2</a>
+    <a href="#title3" class="a-navbar">หัวข้อที่ 3</a>
+</nav>
+
+<div class="div-title" id="title1">
+<h1 class"title-h1">Prarewa Rerkcharern</h1>
 
 </div>
  
     <h2 class="title-h2">Subtitle</h2>
 
-    <p1 class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum laudantium alias aperiam, ratione possimus vel itaque deserunt reiciendis recusandae necessitatibus nisi sunt earum, quam, laborum cupiditate laboriosam. Tempora, iusto voluptatibus.  
+    <p1 class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum laudantium alias aperiam, ratione possimus vel itaque deserunt reiciendis recusandae necessitatibus nisi sunt earum, quam, laborum cupiditate laboriosam. Tempora, iusto voluptatibus.!</p>  
    
-    <h2 class="title-h3">Pls</h2>
+    <h2 class="title-h3">Pls sub</h2>
 
-    <p2 class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quibusdam provident laboriosam voluptatem voluptatum nobis beatae, nemo ipsa dolorem culpa? Doloremque placeat nobis veritatis possimus, quaerat quae quo debitis quas.
+    <p2 class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quibusdam provident laboriosam voluptatem voluptatum nobis beatae, nemo ipsa dolorem culpa? Doloremque placeat nobis veritatis possimus, quaerat quae quo debitis quas.</p>
    
-    <h3 class="title-h4">Ms. prarewa Rerkcharern</h3>
+    <h3 class="title-h4">Ms. Prarewa Rerkcharern</h3>
     
-    <p3 class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non sunt iusto molestiae eius perferendis, aliquid temporibus repudiandae cupiditate dignissimos, numquam nesciunt enim iure fugiat sed modi corporis recusandae, eveniet nihil!
+    <p3 class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non sunt iusto molestiae eius perferendis, aliquid temporibus repudiandae cupiditate dignissimos, numquam nesciunt enim iure fugiat sed modi corporis recusandae, eveniet nihil!</p>
    
-    <p4 class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quasi itaque, esse dolor porro aperiam amet inventore adipisci repellendus, tempore quam id, autem nulla dolore omnis eius voluptate iure repellat.
+    <p4 class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quasi itaque, esse dolor porro aperiam amet inventore adipisci repellendus, tempore quam id, autem nulla dolore omnis eius voluptate iure repellat.</p>
 
 </body>
 
