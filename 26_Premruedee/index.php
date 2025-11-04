d3d8001f0526c0261b15eac97b9bb34282ae6636
prapapan24 <Prapapan6818@gmail.com>
2025-11-04 10:05:18 +0700

    +

--- 26_Premruedee/index.php
+++ 26_Premruedee/index.php
@@ -8,32 +8,33 @@
 </head>
 <body>
 
-    <!--ส่วนหัว-->
-<div class="div-title">
+    <!-- ส่วนหัว -->
+    <div class="div-title">
     <h1 class="title-h1">Premruedee Supharoekmontha</h1>
-</div>   
+    </div>   
 
-    <!--กล่องที่ 1 -->
+    <!-- กล่องที่ 1 -->
     <div class="box box-blue">
         <h2 class="title-h2">Subtitle</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse minima eos vel quasi! Qui provident optio tempore perspiciatis non distinctio dolores! Eum incidunt, porro laboriosam fuga iusto illo ab architecto.</p>
-</div>
+        <p>My name is Premruedee Supharoekmontha but in online you can call me Premruedee it kinda good when you call me by this name</p>
+    </div>
 
-<!--กล่องที่ 2 -->
-<div class="box box-yellow">
+    <!-- กล่องที่ 2 -->
+    <div class="box box-yellow">
     <h2 class="title-h3">Pls sub</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio harum similique rem iste perferendis hic temporibus et obcaecati consequuntur nobis eius expedita placeat magni dolorum maxime molestias, vel numquam corporis?</p>
-</div>
+    </div>
 
-<!--กล่องที่ 3 -->
-<div class="box box-green">
+    <!-- กล่องที่ 3 -->
+    <div class="box box-green">
     <h3 class="title-h4">Ms. Premruedee Supharoekmontha</h3>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates doloremque ipsam maxime sequi dolorem atque eveniet tenetur velit, reiciendis, blanditiis optio ad qui fugiat enim. Aperiam natus eum earum reiciendis.</p>
-</div>
+    </div>
    
-<!--กล่องที่ 4 -->
-<div class="box box-purple">
+    <!-- กล่องที่ 4 -->
+    <div class="box box-purple">
     <h4 class="title-h5">Chompooo</h4>
+    </div>
 
 </body>
 </html>
