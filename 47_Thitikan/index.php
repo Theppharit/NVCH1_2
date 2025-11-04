eee13e16ec4e792e41d1ddf163b02e563f1a6cbe
Vasuthon <sirarmysdaphr@gmail.com>
2025-11-04 10:55:10 +0700

    *

--- 47_Thitikan/index.php
+++ 47_Thitikan/index.php
@@ -1,37 +1,80 @@
-<h1 class="">h1</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-<p class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero eius quis sunt tempore atque quos eligendi fugit id, nulla odio debitis ex delectus ab veniam tenetur neque. Corrupti, et nobis?
-Error vel odio deleniti aspernatur, sit velit nemo! Possimus dolorem fugiat molestias quod odio vero aperiam esse, porro ab libero explicabo, cumque modi quibusdam atque, voluptas et unde rerum nemo.
-Autem ullam modi ipsum magnam suscipit similique, id iste amet ab ex libero velit ratione necessitatibus ut? Deleniti, ex atque esse, ipsa totam incidunt itaque, vel deserunt voluptatum voluptas sint!
-Molestiae asperiores esse aliquid vitae officiis repellat quia veniam aperiam nulla, animi vel possimus. Eos corrupti natus error, laborum ex inventore maiores, odit quia fugiat laudantium possimus optio dolores at!
-Nisi dolores quam a veniam id enim labore, dolorum officia aperiam beatae repellendus consectetur error minus earum, impedit est, molestias officiis maiores culpa? Hic cum facere nemo facilis asperiores quam?
-Architecto dolore consequuntur praesentium ea ullam autem earum, ad nam deserunt voluptatum vel laboriosam laborum quo itaque, omnis porro necessitatibus officiis, beatae assumenda quisquam! Id dolorum atque ab commodi earum.
-Praesentium est labore similique beatae assumenda reiciendis corrupti in, voluptas accusamus deserunt adipisci corporis alias consectetur eaque error eum officiis saepe quo ab cum voluptate eligendi facere. Cum, totam quibusdam!
-Officia quibusdam vero iste omnis laudantium debitis aperiam esse, mollitia hic voluptatem, ipsam ex dolorem? Officiis repudiandae, adipisci, animi quae laudantium nam esse totam odit blanditiis perferendis quod id minima.
-Atque, obcaecati numquam? Dolores eos nesciunt beatae velit dolor perspiciatis voluptatibus labore culpa eius nostrum, nam repellendus autem laboriosam quod optio. Expedita et, molestias assumenda maiores fugit quas obcaecati ab.
-Voluptatum atque ducimus illo vitae nobis natus culpa laudantium, officiis quod sunt aut fugiat debitis in numquam quo porro aliquid. Commodi culpa possimus doloribus aperiam reiciendis consequatur itaque ratione excepturi.</p>
 
-<h2 class="">h2</h2>
 
-<p class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero eius quis sunt tempore atque quos eligendi fugit id, nulla odio debitis ex delectus ab veniam tenetur neque. Corrupti, et nobis?
-Error vel odio deleniti aspernatur, sit velit nemo! Possimus dolorem fugiat molestias quod odio vero aperiam esse, porro ab libero explicabo, cumque modi quibusdam atque, voluptas et unde rerum nemo.
-Autem ullam modi ipsum magnam suscipit similique, id iste amet ab ex libero velit ratione necessitatibus ut? Deleniti, ex atque esse, ipsa totam incidunt itaque, vel deserunt voluptatum voluptas sint!
-Molestiae asperiores esse aliquid vitae officiis repellat quia veniam aperiam nulla, animi vel possimus. Eos corrupti natus error, laborum ex inventore maiores, odit quia fugiat laudantium possimus optio dolores at!
-Nisi dolores quam a veniam id enim labore, dolorum officia aperiam beatae repellendus consectetur error minus earum, impedit est, molestias officiis maiores culpa? Hic cum facere nemo facilis asperiores quam?
-Architecto dolore consequuntur praesentium ea ullam autem earum, ad nam deserunt voluptatum vel laboriosam laborum quo itaque, omnis porro necessitatibus officiis, beatae assumenda quisquam! Id dolorum atque ab commodi earum.
-Praesentium est labore similique beatae assumenda reiciendis corrupti in, voluptas accusamus deserunt adipisci corporis alias consectetur eaque error eum officiis saepe quo ab cum voluptate eligendi facere. Cum, totam quibusdam!
-Officia quibusdam vero iste omnis laudantium debitis aperiam esse, mollitia hic voluptatem, ipsam ex dolorem? Officiis repudiandae, adipisci, animi quae laudantium nam esse totam odit blanditiis perferendis quod id minima.
-Atque, obcaecati numquam? Dolores eos nesciunt beatae velit dolor perspiciatis voluptatibus labore culpa eius nostrum, nam repellendus autem laboriosam quod optio. Expedita et, molestias assumenda maiores fugit quas obcaecati ab.
-Voluptatum atque ducimus illo vitae nobis natus culpa laudantium, officiis quod sunt aut fugiat debitis in numquam quo porro aliquid. Commodi culpa possimus doloribus aperiam reiciendis consequatur itaque ratione excepturi.</p>
-<h3 class="">h3</h3>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/style.css">
 
-<p class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero eius quis sunt tempore atque quos eligendi fugit id, nulla odio debitis ex delectus ab veniam tenetur neque. Corrupti, et nobis?
-Error vel odio deleniti aspernatur, sit velit nemo! Possimus dolorem fugiat molestias quod odio vero aperiam esse, porro ab libero explicabo, cumque modi quibusdam atque, voluptas et unde rerum nemo.
-Autem ullam modi ipsum magnam suscipit similique, id iste amet ab ex libero velit ratione necessitatibus ut? Deleniti, ex atque esse, ipsa totam incidunt itaque, vel deserunt voluptatum voluptas sint!
-Molestiae asperiores esse aliquid vitae officiis repellat quia veniam aperiam nulla, animi vel possimus. Eos corrupti natus error, laborum ex inventore maiores, odit quia fugiat laudantium possimus optio dolores at!
-Nisi dolores quam a veniam id enim labore, dolorum officia aperiam beatae repellendus consectetur error minus earum, impedit est, molestias officiis maiores culpa? Hic cum facere nemo facilis asperiores quam?
-Architecto dolore consequuntur praesentium ea ullam autem earum, ad nam deserunt voluptatum vel laboriosam laborum quo itaque, omnis porro necessitatibus officiis, beatae assumenda quisquam! Id dolorum atque ab commodi earum.
-Praesentium est labore similique beatae assumenda reiciendis corrupti in, voluptas accusamus deserunt adipisci corporis alias consectetur eaque error eum officiis saepe quo ab cum voluptate eligendi facere. Cum, totam quibusdam!
-Officia quibusdam vero iste omnis laudantium debitis aperiam esse, mollitia hic voluptatem, ipsam ex dolorem? Officiis repudiandae, adipisci, animi quae laudantium nam esse totam odit blanditiis perferendis quod id minima.
-Atque, obcaecati numquam? Dolores eos nesciunt beatae velit dolor perspiciatis voluptatibus labore culpa eius nostrum, nam repellendus autem laboriosam quod optio. Expedita et, molestias assumenda maiores fugit quas obcaecati ab.
-Voluptatum atque ducimus illo vitae nobis natus culpa laudantium, officiis quod sunt aut fugiat debitis in numquam quo porro aliquid. Commodi culpa possimus doloribus aperiam reiciendis consequatur itaque ratione excepturi.</p>
+    <title>Thitikan</title>
+</head>
+
+
+
+<body>
+
+
+<nav class="navbar">
+    <a href="#title1" class="a-navbar">หัวข้อที่1</a>
+    <a href="#title2" class="a-navbar">หัวข้อที่2</a>
+    <a href="#title3" class="a-navbar">หัวข้อที่3</a>
+</nav>
+
+<div class="div-title" id="title1">
+<h1 class="title-h1">Thitikan Anchuen</h1>
+
+</div>
+
+    <h2 class="title-h2">Subtitle</h2>
+
+    <p1 class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate eum doloremque exercitationem iusto distinctio soluta. Similique perspiciatis quas quae sit quidem sint, quis ipsa quibusdam possimus cumque aut accusantium voluptatum!</p>
+
+    <h2 class="title-h3">Pls sub</h2>
+
+    <p2 class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro iste dicta id, ullam maxime inventore eaque nisi molestias harum voluptate qui veritatis hic exercitationem. Inventore animi magni eos voluptates in?</p>
+
+    <h3 class="title-h4">Ms.Thitikan Anchuen</h3>
+
+    <p3 class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo a voluptas est natus voluptatum voluptatem itaque, quas id molestias? Corrupti doloribus accusantium ipsam aspernatur non. Ea, rerum accusantium? Et, perferendis.</p>
+
+    <p4 class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, dolores ipsa praesentium laboriosam alias enim veritatis inventore ducimus illum nisi optio ab temporibus rem placeat debitis. Cum sit labore qui!</p>
+
+</body>
+
+
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
