5e89b6a1fdcddced885ad2eb70a2b0599ec7bde3
prarewa2550-prog <prarewa2550@gmail.com>
2025-11-04 12:17:50 +0700

    เสร็จสักทีโว้ยยยยยยยยยยยยยย

--- 21_prarewa/index.php
+++ 21_prarewa/index.php
@@ -1,50 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
-
-
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prarewa Rerkcharern</title>
     <link rel="stylesheet" href="./assets/style.css">
-
-    <title>Prarewa</title>
+    <title> Prarewa</title>
 </head>
 
 <body>
-    
-
-
 <nav class="navbar">
     <a href="#title1" class="a-navbar">หัวข้อที่ 1</a>
     <a href="#title2" class="a-navbar">หัวข้อที่ 2</a>
     <a href="#title3" class="a-navbar">หัวข้อที่ 3</a>
 </nav>
 
-<div class="div-title" id="title1">
-<h1 class"title-h1">Prarewa Rerkcharern</h1>
-<h1 class"title-h5">h1</h1>
 
+
+    <!-- ส่วนหัว -->
+<div class="div-title">
+    <h1 class="title-h1" id="title">Prarewa Rerkcharern</h1>
+</div>   
+
+    <!-- กล่องที่ 1 -->
+<div class="box box-blue">
+        <h2 class="title-h2" id="title2">Subtitle</h2>
+        <p>My name is Prarewa Rerkcharern </p>
 </div>
- 
-    <h2 class="title-h2">Subtitle</h2>
 
-    <p1 class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum laudantium alias aperiam, ratione possimus vel itaque deserunt reiciendis recusandae necessitatibus nisi sunt earum, quam, laborum cupiditate laboriosam. Tempora, iusto voluptatibus.!</p>  
-   
-    <h2 class="title-h3">Pls sub</h2>
+    <!-- กล่องที่ 2 -->
+<div class="box box-yellow">
+    <h2 class="title-h3"id="title3">Pls sub</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio harum similique rem iste perferendis hic temporibus et obcaecati consequuntur nobis eius expedita placeat magni dolorum maxime molestias, vel numquam corporis?</p>
+</div>
 
-    <p2 class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quibusdam provident laboriosam voluptatem voluptatum nobis beatae, nemo ipsa dolorem culpa? Doloremque placeat nobis veritatis possimus, quaerat quae quo debitis quas.</p>
+    <!-- กล่องที่ 3 -->
+<div class="box box-green">
+    <h3 class="title-h4"id="title4">Ms. Prarewa Rerkcharern</h3>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates doloremque ipsam maxime sequi dolorem atque eveniet tenetur velit, reiciendis, blanditiis optio ad qui fugiat enim. Aperiam natus eum earum reiciendis.</p>
+</div>
    
-    <h3 class="title-h4">Ms. Prarewa Rerkcharern</h3>
-    
-    <p3 class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non sunt iusto molestiae eius perferendis, aliquid temporibus repudiandae cupiditate dignissimos, numquam nesciunt enim iure fugiat sed modi corporis recusandae, eveniet nihil!</p>
-   
-    <p4 class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quasi itaque, esse dolor porro aperiam amet inventore adipisci repellendus, tempore quam id, autem nulla dolore omnis eius voluptate iure repellat.</p>
+    <!-- กล่องที่ 4 -->
+ <div class="box box-purple">
+    <h4 class="title-h5">BAMBOO</h4>
+ </div>
 
 </body>
-
-
-
-
-
 </html>
