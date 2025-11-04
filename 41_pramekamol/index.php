2cf04ac88c65994cf7da5564f63fd0a62753abf6
aunchulee44 <pang32358@gmail.com>
2025-11-04 09:18:51 +0700

    +

--- 41_pramekamol/index.php
+++ 41_pramekamol/index.php
@@ -6,14 +6,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< Updated upstream
     <link rel="stylesheet" href="./assets/style.css">
 
+=======
+    <link rel="stylesheet" href="./assets/styie.css">
+>>>>>>> Stashed changes
     <title>pramekamol</title>
+   
 </head>
 
 
 
 <body>
+<<<<<<< Updated upstream
 
 
 <div class="div-title">
@@ -68,7 +74,47 @@
         Et quo saepe numquam eaque laborum beatae cum voluptatibus, fuga aspernatur amet ab. Molestias modi totam animi consectetur placeat fugit optio facere architecto quidem iste quibusdam, corrupti magnam eligendi debitis?</p>
 
 
+=======
+  
+   
+    <div class="div-title">
+        <h1 class="title-h1"><h1></h1>
+ <div>
+>>>>>>> Stashed changes
 
+
+ 
+    
+<h2 class="title-h2">subtitle</h2>
+
+<p class="1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quo eveniet, vel quis doloremque natus possimus minima corrupti necessitatibus totam, adipisci odio autem ipsum, excepturi amet perspiciatis facere maiores quos.
+Unde tempora praesentium, harum doloribus similique nulla eius! Dignissimos eum itaque maxime voluptatem consectetur omnis nostrum eos temporibus magni, reprehenderit inventore iure debitis ducimus eius consequuntur error. Assumenda, consectetur sit.
+Sed voluptates quam voluptatibus nostrum ipsam quia doloribus perferendis reprehenderit molestias magni nesciunt veritatis quaerat iste, facilis, architecto minus tempore fuga temporibus. Accusantium laudantium voluptatem odio, ea suscipit alias corrupti.
+Culpa dolorem architecto iure, quis voluptatum ut amet. Pariatur numquam consectetur alias tempora eaque, illo ipsam facilis iusto amet est laudantium atque officia illum molestias dolore nihil libero? Dolor, id!
+Modi quis eaque sunt distinctio minus explicabo, eius quam esse cumque nihil? Ex eligendi doloribus vel enim rem atque error maiores amet aliquid ullam, recusandae natus sunt ipsam! Atque, alias?
+Sunt magni sapiente natus esse laudantium itaque id suscipit omnis assumenda voluptates exercitationem ea ex aliquid fugiat numquam atque ipsa corporis sequi perferendis, repudiandae obcaecati delectus, veniam eligendi repellat! Iure.
+Nam porro, repellat eum, minus tenetur illo non a culpa voluptatibus maxime mollitia consequuntur voluptates excepturi iste soluta vero deleniti perspiciatis facilis? Eveniet excepturi vel reprehenderit ex quo praesentium temporibus.
+Provident nostrum, ut consequatur pariatur inventore excepturi, libero dolor ab deserunt unde aperiam labore dolorem, commodi a voluptatem animi ea recusandae sit harum molestias amet. Officiis odio neque aperiam rem.</p>
+
+<h2 class="">h2</h2>
+<p class="1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate rerum corporis, harum at nulla nesciunt aut quo sunt dolor, eligendi, quisquam dolorem placeat corrupti facere. Omnis quibusdam nesciunt asperiores. Recusandae!
+Enim nihil perspiciatis beatae quisquam saepe. Excepturi dicta assumenda repellat culpa sed vel error reiciendis? Obcaecati cum nisi neque ratione nostrum unde, animi est illo incidunt, corrupti delectus et totam.
+Alias illo recusandae voluptates tempora, molestiae illum. Reprehenderit sed dolor, cupiditate sint obcaecati non harum explicabo. Nostrum vitae quas distinctio dicta. Cum excepturi perferendis, velit aperiam maiores vero delectus consequatur!
+Optio asperiores labore, ducimus maiores nulla tenetur enim debitis porro, quasi aperiam totam pariatur itaque exercitationem recusandae aliquam. Recusandae odio, hic cupiditate nisi dolor atque! Sint quisquam perferendis eos velit?
+Nulla reprehenderit maiores consequatur beatae unde aut a facilis ipsam dignissimos necessitatibus hic, magnam odio non odit ratione tenetur quos quasi commodi, nemo molestias! Veritatis quaerat quo obcaecati corporis nobis!
+Dolor corrupti, aliquam, dicta maiores provident numquam reiciendis, in sit tempore expedita ut quasi quo. Rem distinctio natus nulla commodi deleniti, quae blanditiis aut voluptas adipisci sapiente labore perferendis eaque?
+Rerum expedita ducimus quis harum nam molestias, odio cumque voluptate modi ullam labore dolorem cupiditate et consequatur ratione, perferendis eveniet praesentium deserunt officia provident dolore error? Molestias, assumenda pariatur. Unde.
+Beatae quisquam modi necessitatibus sunt, temporibus at qui deleniti ullam, eos rerum nobis in. Error non placeat dolorem perferendis dicta eligendi quo dolore corporis. Ullam assumenda reprehenderit quis in deleniti?</p>
+
+<h3 class="">h3</h3>
+<p class="1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis corporis aut fugiat ex ducimus corrupti earum magnam nobis doloremque quo illum at quidem labore ipsam, odio inventore quaerat totam ad.
+Vel tempora asperiores iste autem cum, consectetur error minus, delectus laborum a reprehenderit dolore quasi, maxime esse maiores adipisci quam voluptate. Dolore veritatis, rem numquam inventore voluptate autem harum iste!
+Quae neque aliquam quidem illum, tempora, aut error dolores eaque similique maxime velit alias corporis molestiae quisquam, quaerat veritatis quis saepe nihil delectus reiciendis placeat. Beatae, eos. Perferendis, eum molestias!
+Quisquam vero numquam blanditiis, soluta iste in deleniti rerum veniam cumque, necessitatibus quos repudiandae quia non adipisci, a officia quas doloribus error? Recusandae accusamus ex enim dignissimos, veritatis minima molestiae.
+Quod veniam incidunt delectus totam dolorum dolores nulla ipsa aspernatur provident soluta architecto amet, fugiat in optio officiis iusto. Commodi placeat corporis similique deleniti voluptates, non eos adipisci eligendi velit.
+Laboriosam, molestiae dolor. Iste dolorum eveniet debitis explicabo temporibus consectetur. Rerum vel, voluptates aut quae, amet vitae minus quos eos libero facere quas aperiam. Laboriosam corporis amet voluptates quis dolor?
+Laudantium, dolor omnis? Quae repellat minima consectetur quo nemo incidunt nihil debitis obcaecati facilis commodi, itaque molestiae maiores a distinctio magni dolorum vitae quidem omnis possimus. Quidem velit nam nostrum?
+Quaerat aspernatur doloremque assumenda vitae explicabo enim, labore non reprehenderit dicta temporibus, saepe iste delectus perferendis repellat molestiae iusto officiis est dolor ea officia perspiciatis? Vel in quis porro eveniet.</p>
 </body>
 
 
