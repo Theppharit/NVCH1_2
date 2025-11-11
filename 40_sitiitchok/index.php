958f597a7a9e48d22309232a374227ab674098b5
PanatWongsa <anyapach2111@gmail.com>
2025-11-11 10:22:36 +0700

    +

--- 40_sitiitchok/index.php
+++ 40_sitiitchok/index.php
@@ -19,7 +19,7 @@
 
 <body>
 
-
+<!-- Navbar -->
 <nav class="navbar">
     <a href="#title1" class="a-navbar">หัวข้อที่ 1</a>
     <a href="#title2" class="a-navbar">หัวข้อที่ 2</a>
@@ -27,13 +27,13 @@
 </nav>
    
 
-
+<!-- Title -->
     <div class="div-title"id="title1"> 
     <h1 class="title-h1">h1</h1>
     </div>
-
+<section class="section-content">
     <img src="./assets/img/1.jpg" alt="" class="">
-<iframe width="560" height="315" src="https://www.youtube.com/embed/19Np05qlakg?si=VkJCB4dBUcTHAgww&amp;start=427" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/19Np05qlakg?si=VkJCB4dBUcTHAgww&amp;start=427" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d413070.7985719974!2d138.68187203650905!3d36.0174700590473!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60192a8d477b01e9%3A0xfed91267aa80526c!2z4LiI4Lix4LiH4Lir4Lin4Lix4LiU4LmE4LiL4LiV4Liw4Lih4LiwIOC4jeC4teC5iOC4m-C4uOC5iOC4mQ!5e0!3m2!1sth!2sth!4v1762828004962!5m2!1sth!2sth" width="560" height="315" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
@@ -49,6 +49,7 @@
         Asperiores molestiae porro, omnis sint quasi delectus corrupti voluptate laborum atque, explicabo consequatur nulla culpa reiciendis sed tempore? Dolore inventore fuga corporis exercitationem atque? Dolore incidunt cum laborum consequuntur id!
         Ea minima illo porro saepe maxime cum, architecto in ducimus mollitia ad et optio similique tenetur libero alias voluptas, sequi dolor totam vel error debitis rem nihil? Consequatur, ducimus optio?
         Et quo saepe numquam eaque laborum beatae cum voluptatibus, fuga aspernatur amet ab. Molestias modi totam animi consectetur placeat fugit optio facere architecto quidem iste quibusdam, corrupti magnam eligendi debitis?</p>
+    </section>
 
     <div class="div-title"id="title2">
     <h1 class="title-h1">h2</h2>
