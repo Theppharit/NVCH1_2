8aea2d523a4e2ccfa71c6c0a9fd7fb39e587472f
PanatWongsa <pataratorthinthonghang@gmail.com>
2025-11-11 10:34:59 +0700

    +

--- 39_pataraton/index.php
+++ 39_pataraton/index.php
@@ -33,13 +33,14 @@
     <h1 class="title-h1">h1</h1>
     </div>
 
-<img src="./assets/img/1.jpg" alt="" class=""> 
+<section ciass="section-content">
+    <img src="./assets/img/1.jpg" alt="" class=""> 
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/ftZoPae7r0E?si=xToiXDWeK3DDN55V" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/ftZoPae7r0E?si=xToiXDWeK3DDN55V" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d75993.08981340448!2d-2.30586271428048!3d53.47231928806191!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487a4d4c5226f5db%3A0xd9be143804fe6baa!2z4LmB4Lih4LiZ4LmA4LiK4Liq4LmA4LiV4Lit4Lij4LmMIOC4quC4q-C4o-C4suC4iuC4reC4suC4k-C4suC4iOC4seC4geC4ow!5e0!3m2!1sth!2sth!4v1762828044288!5m2!1sth!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>    
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d75993.08981340448!2d-2.30586271428048!3d53.47231928806191!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487a4d4c5226f5db%3A0xd9be143804fe6baa!2z4LmB4Lih4LiZ4LmA4LiK4Liq4LmA4LiV4Lit4Lij4LmMIOC4quC4q-C4o-C4suC4iuC4reC4suC4k-C4suC4iOC4seC4geC4ow!5e0!3m2!1sth!2sth!4v1762828044288!5m2!1sth!2sth" width="560" height="315" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>    
 
-<h2 class="title-h2">Subtitle</h2>
+    <h2 class="title-h2">Subtitle</h2>
 
     <p class="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam quidem reprehenderit iusto in et, nesciunt recusandae omnis eos rerum distinctio aliquid nisi suscipit nostrum incidunt reiciendis. Reprehenderit unde suscipit mollitia?
         Adipisci beatae, quae voluptatum at officiis, molestiae voluptate sunt itaque sapiente, quam excepturi asperiores reprehenderit ipsum veniam. Quia sit aliquid commodi nam, adipisci assumenda, eveniet veritatis ipsam aspernatur deleniti dolorem!
@@ -51,6 +52,7 @@
         Asperiores molestiae porro, omnis sint quasi delectus corrupti voluptate laborum atque, explicabo consequatur nulla culpa reiciendis sed tempore? Dolore inventore fuga corporis exercitationem atque? Dolore incidunt cum laborum consequuntur id!
         Ea minima illo porro saepe maxime cum, architecto in ducimus mollitia ad et optio similique tenetur libero alias voluptas, sequi dolor totam vel error debitis rem nihil? Consequatur, ducimus optio?
         Et quo saepe numquam eaque laborum beatae cum voluptatibus, fuga aspernatur amet ab. Molestias modi totam animi consectetur placeat fugit optio facere architecto quidem iste quibusdam, corrupti magnam eligendi debitis?</p>
+</section>
 
     <div class="div-title"id="title2">
     <h1 class="title-h1">h2</h2>
@@ -86,8 +88,6 @@
         Ea minima illo porro saepe maxime cum, architecto in ducimus mollitia ad et optio similique tenetur libero alias voluptas, sequi dolor totam vel error debitis rem nihil? Consequatur, ducimus optio?
         Et quo saepe numquam eaque laborum beatae cum voluptatibus, fuga aspernatur amet ab. Molestias modi totam animi consectetur placeat fugit optio facere architecto quidem iste quibusdam, corrupti magnam eligendi debitis?</p>
 
-
-id="title3
 </body>
 
 
