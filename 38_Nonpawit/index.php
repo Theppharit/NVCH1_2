eff2f63904b2ba94ed96c10829d132eef3a32fe3
nxghtz <dekdgamer348@gmail.com>
2025-11-11 09:42:08 +0700

    Update index.php

--- 38_Nonpawit/index.php
+++ 38_Nonpawit/index.php
@@ -6,8 +6,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/style.css">
 
+    <!-- add css -->
+    <link rel="stylesheet" href="./assets/style.css">
+    <!-- web name -->
     <title>Nonpawit</title>
 </head>
 
