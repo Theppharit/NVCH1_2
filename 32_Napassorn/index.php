fc7e63dcc70f3e22f102d9b72b6f2f4651ffd9f2
Kittipop0 <napassornnoii2549@gmail.com>
2025-11-11 10:19:21 +0700

    +

--- 32_Napassorn/index.php
+++ 32_Napassorn/index.php
@@ -3,28 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Web Name -->
     <title>Napassorn Karnreaw</title>
+
+
+    <!-- Add Css -->
     <link rel="stylesheet" href="asset/style.css">
 </head>
-<body>
 
+
+
+<body>
+<!-- Navbar -->
 <nav class="navbar">
     <a href="#title1" class="a-navbar">หัวข้อที่ 1</a>
     <a href="#title2" class="a-navbar">หัวข้อที่ 2</a>
     <a href="#title3" class="a-navbar">หัวข้อที่ 3</a>
 </nav>
 
+
+
+<!-- Title -->
     <!-- ส่วนหัว -->
    <div class="div-title" id="title 1">
         <h1 class="title-h1" id="title1">Napassorn Karnreaw</h1>
     </div>
-    <!-- กล่องที่ 1 -->
-    <div class="box box-blue">
-        <h2 class="title-h2" id="title2">Halooooooo</h2>
-        
-        <img src="./asset/img/2.jpg" alt=""Class="">
 
-   <p class="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam eaque sapiente fugit a corporis molestiae facere pariatur, obcaecati, eveniet asperiores consectetur deleniti. Soluta veritatis, quae dolores totam explicabo ab non.
+
+<section class="secyion-content"> 
+    
+    <h2 class="title-h2" id="title2">Halooooooo</h2>
+
+    <img src="./asset/img/2.jpg" alt=""class="">
+   
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/m6CXjvXU3M0?si=erRgrMlxeV4sp8KL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+   
+   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3844.7958816651408!2d102.081187675124!3d15.495407785104014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3118d527f8f93e53%3A0xeeba96c1cfb86f83!2z4LmC4Lij4LiH4LmA4Lij4Li14Lii4LiZ4Lib4Lij4Liy4LiH4LiE4LmM4LiX4Lit4LiH4Lin4Li04LiX4Lii4Liy!5e0!3m2!1sth!2sth!4v1762827785925!5m2!1sth!2sth" width="560" height="315" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  
+    <p class="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam eaque sapiente fugit a corporis molestiae facere pariatur, obcaecati, eveniet asperiores consectetur deleniti. Soluta veritatis, quae dolores totam explicabo ab non.
    Ipsum veritatis repellendus quae repudiandae voluptatem! Accusantium nemo perferendis voluptate, saepe mollitia quo, quis est voluptas ullam sapiente facere exercitationem laudantium earum vitae ea iure fugit fugiat corporis numquam! Earum?
    Ea sunt inventore iure magni suscipit eius sit libero rem minima dicta eligendi, quisquam tempore quaerat et nam id labore nobis? Totam saepe illo vitae quia veritatis ducimus, a ratione?
    Reprehenderit tempore recusandae, explicabo neque, culpa aut at assumenda similique reiciendis totam eius excepturi molestiae nam accusantium inventore eos suscipit vitae vel atque incidunt. Maiores minima id doloremque quasi illum?
@@ -34,6 +51,14 @@
    Aperiam architecto, nisi nemo omnis temporibus quae explicabo aspernatur earum ab fugiat odio voluptatem optio doloribus quisquam vitae modi quis qui in! Eos reprehenderit nam corporis ab? Libero, dolore voluptates?
    Quasi quas perspiciatis laboriosam eius et vel at aliquam vitae debitis neque unde rerum est architecto iure harum a asperiores, quo consectetur incidunt, eaque, aut itaque quaerat! Iure, eos error!
    Aspernatur, sunt maiores minima quasi doloribus qui officia quae reiciendis optio accusamus eum veritatis natus et tempora unde error obcaecati consequuntur nihil. Repellat asperiores earum nam, eaque eum impedit magni.</p>
+       
+   <!-- กล่องที่ 1 -->
+    <div class="box box-blue">
+
+   </section>
+       
+
+    
 
     <!-- กล่องที่ 2 -->
     <div class="box box-yellow">
