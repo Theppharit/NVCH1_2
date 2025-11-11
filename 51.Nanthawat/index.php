b2e574fe637b7cd35e6a9d2f07c9fc44d54df05f
onigiri02949 <onigiri02949@gmail.com>
2025-11-11 09:21:04 +0700

    +

--- 51.Nanthawat/index.php
+++ 51.Nanthawat/index.php
@@ -11,7 +11,7 @@
 
 <img src="./assets/img/3.jpg" alt="" class=""> //คำสั่งรูปภาพ
 
-
+<iframe width="560" height="315" src="https://www.youtube.com/embed/nsM63Kgh3RQ?si=_64J9MZjdggZmGLV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
 
 <body>
