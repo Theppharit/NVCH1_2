284e3ec21a9838aac0ead8b355eecaf5371f1674
aunchulee44 <239268383+yingprame14-design@users.noreply.github.com>
2025-11-11 09:21:31 +0700

    +

--- 41_pramekamol/index.php
+++ 41_pramekamol/index.php
@@ -32,6 +32,8 @@
 
 <img src="./assets/img/2.jpg" alt="" class="">
 
+<iframe width="560" height="315" src="https://www.youtube.com/embed/hBK29bbOLS4?si=vGd8ZYf99AMSeufo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
 
 <h2 class="title-h2">Subtitle</h2>
 
