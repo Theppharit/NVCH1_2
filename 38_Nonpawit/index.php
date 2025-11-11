899baabd1b90f75b8191983e1f0437d98c3344a1
nxghtz <dekdgamer348@gmail.com>
2025-11-11 10:33:43 +0700

    +

--- 38_Nonpawit/index.php
+++ 38_Nonpawit/index.php
@@ -30,21 +30,24 @@
 
 
 
+    
     <!-- Title -->
     <div class="div-title" id="title1">
         <h1 class="title-h1">h1</h1>
     </div>
 
-    <h2 class="title-h2">Subtitle</h2>
+    <section class="section-content">
 
-    <img src="./assets/img/2.jpg" alt="" class="">
+        <h2 class="title-h2">Subtitle</h2>
 
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/lWhygAYsPrc?si=0yf4gNlnUng9G6jA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <img src="./assets/img/2.jpg" alt="" class="">
 
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2377.1042935544683!2d-2.9633923227321444!3d53.43084347231397!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487b21654b02538b%3A0x84576a57e21973ff!2z4LmB4Lit4LiZ4Lif4Li14Lil4LiU4LmM!5e0!3m2!1sth!2sth!4v1762828001417!5m2!1sth!2sth" width="560" height="315" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/lWhygAYsPrc?si=0yf4gNlnUng9G6jA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2377.1042935544683!2d-2.9633923227321444!3d53.43084347231397!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487b21654b02538b%3A0x84576a57e21973ff!2z4LmB4Lit4LiZ4Lif4Li14Lil4LiU4LmM!5e0!3m2!1sth!2sth!4v1762828001417!5m2!1sth!2sth" width="560" height="315" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 
-    <p class="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam quidem reprehenderit iusto in et, nesciunt recusandae omnis eos rerum distinctio aliquid nisi suscipit nostrum incidunt reiciendis. Reprehenderit unde suscipit mollitia?
+        <p class="">LLorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam quidem reprehenderit iusto in et, nesciunt recusandae omnis eos rerum distinctio aliquid nisi suscipit nostrum incidunt reiciendis. Reprehenderit unde suscipit mollitia?
         Adipisci beatae, quae voluptatum at officiis, molestiae voluptate sunt itaque sapiente, quam excepturi asperiores reprehenderit ipsum veniam. Quia sit aliquid commodi nam, adipisci assumenda, eveniet veritatis ipsam aspernatur deleniti dolorem!
         Illum dignissimos corporis recusandae quibusdam laboriosam vel non odio delectus explicabo earum nam quasi, obcaecati sequi minima eveniet quis soluta nostrum sunt! Quasi fugit eos eaque expedita dolores tempora modi.
         Consequatur omnis nemo at, possimus distinctio sequi labore repellendus natus ipsum exercitationem. Velit provident, repudiandae quae sed quisquam voluptates praesentium repellat atque natus rem dolores nesciunt perferendis nisi error facilis!
@@ -54,16 +57,16 @@
         Asperiores molestiae porro, omnis sint quasi delectus corrupti voluptate laborum atque, explicabo consequatur nulla culpa reiciendis sed tempore? Dolore inventore fuga corporis exercitationem atque? Dolore incidunt cum laborum consequuntur id!
         Ea minima illo porro saepe maxime cum, architecto in ducimus mollitia ad et optio similique tenetur libero alias voluptas, sequi dolor totam vel error debitis rem nihil? Consequatur, ducimus optio?
         Et quo saepe numquam eaque laborum beatae cum voluptatibus, fuga aspernatur amet ab. Molestias modi totam animi consectetur placeat fugit optio facere architecto quidem iste quibusdam, corrupti magnam eligendi debitis?</p>
-
+    </section>
 
 
     <div class="div-title" id="title2">
         <h1 class="title-h1">h1-2</h1>
     </div>
 
-    <h2 class="title-h2">Subtitle</h2>
+        <h2 class="title-h2">Subtitle</h2>
 
-    <p class="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam quidem reprehenderit iusto in et, nesciunt recusandae omnis eos rerum distinctio aliquid nisi suscipit nostrum incidunt reiciendis. Reprehenderit unde suscipit mollitia?
+        <p class="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam quidem reprehenderit iusto in et, nesciunt recusandae omnis eos rerum distinctio aliquid nisi suscipit nostrum incidunt reiciendis. Reprehenderit unde suscipit mollitia?
         Adipisci beatae, quae voluptatum at officiis, molestiae voluptate sunt itaque sapiente, quam excepturi asperiores reprehenderit ipsum veniam. Quia sit aliquid commodi nam, adipisci assumenda, eveniet veritatis ipsam aspernatur deleniti dolorem!
         Illum dignissimos corporis recusandae quibusdam laboriosam vel non odio delectus explicabo earum nam quasi, obcaecati sequi minima eveniet quis soluta nostrum sunt! Quasi fugit eos eaque expedita dolores tempora modi.
         Consequatur omnis nemo at, possimus distinctio sequi labore repellendus natus ipsum exercitationem. Velit provident, repudiandae quae sed quisquam voluptates praesentium repellat atque natus rem dolores nesciunt perferendis nisi error facilis!
@@ -80,9 +83,9 @@
         <h1 class="title-h1">h1-3</h1>
     </div>
 
-    <h2 class="title-h2">Subtitle</h2>
+        <h2 class="title-h2">Subtitle</h2>
 
-    <p class="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam quidem reprehenderit iusto in et, nesciunt recusandae omnis eos rerum distinctio aliquid nisi suscipit nostrum incidunt reiciendis. Reprehenderit unde suscipit mollitia?
+        <p class="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam quidem reprehenderit iusto in et, nesciunt recusandae omnis eos rerum distinctio aliquid nisi suscipit nostrum incidunt reiciendis. Reprehenderit unde suscipit mollitia?
         Adipisci beatae, quae voluptatum at officiis, molestiae voluptate sunt itaque sapiente, quam excepturi asperiores reprehenderit ipsum veniam. Quia sit aliquid commodi nam, adipisci assumenda, eveniet veritatis ipsam aspernatur deleniti dolorem!
         Illum dignissimos corporis recusandae quibusdam laboriosam vel non odio delectus explicabo earum nam quasi, obcaecati sequi minima eveniet quis soluta nostrum sunt! Quasi fugit eos eaque expedita dolores tempora modi.
         Consequatur omnis nemo at, possimus distinctio sequi labore repellendus natus ipsum exercitationem. Velit provident, repudiandae quae sed quisquam voluptates praesentium repellat atque natus rem dolores nesciunt perferendis nisi error facilis!
