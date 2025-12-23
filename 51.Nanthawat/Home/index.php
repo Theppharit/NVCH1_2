842093e68d82ea2e69a13748ab0c943b29ffd998
onigiri02949 <onigiri02949@gmail.com>
2025-12-23 10:37:26 +0700

    +

--- 51.Nanthawat/Home/index.php
+++ 51.Nanthawat/Home/index.php
@@ -136,19 +136,19 @@
         <section class="logos section">
             <div class="logos__container container grid">
                 <div class="logos__img">
-                    <img src="assets/img/logo1.png" alt="">
+                    <img src="assets/img/100.png" alt="">
                 </div>
 
                 <div class="logos__img">
-                    <img src="assets/img/logo2.png" alt="">
+                    <img src="assets/img/200.png" alt="">
                 </div>
 
                 <div class="logos__img">
-                    <img src="assets/img/logo3.png" alt="">
+                    <img src="assets/img/300.png" alt="">
                 </div>
 
                 <div class="logos__img">
-                    <img src="assets/img/logo4.png" alt="">
+                    <img src="assets/img/400.png" alt="">
                 </div>
             </div>
         </section>
