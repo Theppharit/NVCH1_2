238b5351b0c83aff975dab270afeec7f2d17b1a6
Vasuthon <sirarmysdaphr@gmail.com>
2025-12-23 09:46:08 +0700

    555

--- 47_Thitikan/Login Form/assets/js/main.js
+++ 47_Thitikan/Login Form/assets/js/main.js
@@ -10,8 +10,23 @@ iconEye.addEventListener('click', () =>{
     iconEye.classList.toggle('ri-eye-line')                          
 })
 }
-
-
 showHiddenPass('loginPass','loginEye')
 
 /*=============== SWIPER IMAGES ===============*/
+const swiperLogin = new Swiper('.login__swiper', {
+   loop: true,
+   spaceBetween: '24',
+   grabCursor: true,
+   speed: 600,
+   // effect: 'fade',
+
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+
+   autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+   },
+})
